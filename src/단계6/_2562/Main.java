774df06ec6f59f74ad333f380d65a4614b99c389
YouHyeonJun <collin7202@gmail.com>
2020-07-25 20:07:09 +0900

    최대값

---
+++ src/단계6/_2562/Main.java
@@ -0,0 +1,28 @@
+package 단계6._2562;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		int max = 0;
+		int index = 0;
+
+		for (int i = 0; i < 9; i++) {
+			int val = Integer.parseInt(bufferedReader.readLine());
+
+			if (val > max) {
+				max = val;
+				index = i + 1;
+			}
+
+		}
+
+		System.out.println(max + "\n" + index);
+	}
+}
