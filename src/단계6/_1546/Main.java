f9df3756e86f208840417fd12aefd1b5a15721b4
YouHyeonJun <collin7202@gmail.com>
2020-07-28 21:10:10 +0900

    평균

--- src/단계6/_1546/Main.java
+++ src/단계6/_1546/Main.java
@@ -1,10 +1,8 @@
 package 단계6._1546;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
