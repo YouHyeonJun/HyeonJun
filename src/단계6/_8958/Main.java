003b1cfa2af95710d9f41b1a2f2a821547cb2632
YouHyeonJun <collin7202@gmail.com>
2020-07-29 23:34:26 +0900

    OX 문제

---
+++ src/단계6/_8958/Main.java
@@ -0,0 +1,42 @@
+package 단계6._8958;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int testNum = Integer.parseInt(bufferedReader.readLine());
+		String str[] = new String[testNum];
+
+		for (int i = 0; i < testNum; i++) {
+
+			int count = 0;
+			int sum = 0;
+
+			str[i] = bufferedReader.readLine();
+
+			for (int j = 0; j < str[i].length(); j++) {
+
+				if (str[i].charAt(j) == 'O') {
+					count++;
+				} else {
+					count = 0;
+				}
+				sum += count;
+			}
+
+			bufferedWriter.write(sum + "\n");
+		}
+
+		bufferedWriter.flush();
+		bufferedWriter.close();
+	}
+}
