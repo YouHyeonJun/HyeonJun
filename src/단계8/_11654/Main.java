fcde4696f78bf0157d82a4e54fc069b68536ea76
YouHyeonJun <collin7202@gmail.com>
2020-08-04 23:37:28 +0900

    아스키 코드

---
+++ src/단계8/_11654/Main.java
@@ -0,0 +1,12 @@
+package 단계8._11654;
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+
+		int a = System.in.read();
+		System.out.print(a);
+
+	}
+}
